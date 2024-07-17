68492fced50e25523583162068aa7fa45b52bf51
PRAMOD <pammunaik99.51solution@gmail.com>
2024-07-18 02:21:42 +0530

    Added Tokens Info

---
+++ Lab Programs/Example/Example.java
@@ -0,0 +1,12 @@
+package Example;
+public class Example { // keyword: public, class; identifier: Example; separator: {
+    public static void main(String[] args) { // keywords: public, static, void; identifier: main, String, args; separator: (), {}
+        int number = 42; // keyword: int; identifier: number; literal: 42; operator: =
+        double pi = 3.14; // keyword: double; identifier: pi; literal: 3.14; operator: =
+        boolean isJavaFun = true; // keyword: boolean; identifier: isJavaFun; literal: true; operator: =
+
+        if (isJavaFun) { // keyword: if; identifier: isJavaFun; separator: ()
+            System.out.println("Java is fun!"); // identifier: System, out, println; literal: "Java is fun!"; separator: ();
+        } // separator: }
+    } // separator: }
+} // separator: }
