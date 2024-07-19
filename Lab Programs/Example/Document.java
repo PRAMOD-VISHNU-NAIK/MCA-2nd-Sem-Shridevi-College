fe356cd37eb5c064814e404530148082a3343328
PRAMOD <pammunaik99.51solution@gmail.com>
2024-07-20 00:33:37 +0530

    Modified PPT

---
+++ Lab Programs/Example/Document.java
@@ -0,0 +1,29 @@
+package Example;
+
+/**
+ * I am writing this program to test the Javadoc.
+ * <p>
+ * This class currently includes only the main method where the execution
+ * starts.
+ * </p>
+ * 
+ * @version 1.0
+ * @since 2024-07-19
+ */
+public class Document {
+    /**
+     * This is the main method which makes use of boolean data types and checks the
+     * conditon using if.
+     * 
+     * 
+     * @param args Unused.
+     */
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+
+        if (isJavaFun) {
+            System.out.println("Java is fun!");
+        }
+    }
+
+}
--- Lab Programs/Example/Example.java
+++ Lab Programs/Example/Example.java
@@ -1,15 +1,13 @@
-// package Example;
-// public class Example { // keyword: public, class; identifier: Example; separator: {
-//     public static void main(String[] args) { // keywords: public, static, void; identifier: main, String, args; separator: (), {}
-//         int number = 42; // keyword: int; identifier: number; literal: 42; operator: =
-//         double pi = 3.14; // keyword: double; identifier: pi; literal: 3.14; operator: =
-//         boolean isJavaFun = true; // keyword: boolean; identifier: isJavaFun; literal: true; operator: =
+package Example;
 
-//         if (isJavaFun) { // keyword: if; identifier: isJavaFun; separator: ()
-//             System.out.println("Java is fun!"); // identifier: System, out, println; literal: "Java is fun!"; separator: ();
-//         } // separator: }
-//     } // separator: }
-// } // separator: }
+public class Example {
+    public static void main(String[] args) {
 
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        int y = 10;
 
-javac HelloWorld.java
+    }
+}
